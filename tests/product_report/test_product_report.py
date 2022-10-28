53801cc74b4c44a0542bdbb800ae5111aaf218e8
Jacqueline Silva <jacque.sx@hotmail.com>
2022-10-28 18:01:10 -0300

    :test_tube: test(test_product_report): implementa teste do relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.product.test_product import phrase, mock_product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        mock_product["id"],
+        mock_product["produto"],
+        mock_product["empresa"],
+        mock_product["fabricacao"],
+        mock_product["validade"],
+        mock_product["n_serie"],
+        mock_product["inst"],
+    )
+
+    assert product.__repr__() == phrase
