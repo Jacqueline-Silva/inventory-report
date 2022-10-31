aa0c91394768678c3fb3231432c0e1975b204522
Jacqueline Silva <jacque.sx@hotmail.com>
2022-10-31 17:26:11 -0300

    :sparkles: feat(main): implementa função "Main" do módulo "inventory_report"
    A função recebe por linha de comando o caminho de um arquivo e o tipo de relatório que deseja e retorna o relatório correto de acordo com a extensão do arquivo passado e o seu tipo.

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,26 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    importers_file = {
+        "csv": CsvImporter,
+        "json": JsonImporter,
+        "xml": XmlImporter
+    }
+    try:
+        # ex: inventory_report, '/arq.csv', 'simples'
+        path, type_report = sys.argv[1:]
+        importer = path.split('.')[1]
+        file = InventoryRefactor(importers_file[importer])
+        sys.stdout.write(file.import_data(path, type_report))
+    except ValueError:
+        sys.stderr.write("Verifique os argumentos\n")
+
+
+# stdout = print
+# stderr = exceções
+# stdin = input
